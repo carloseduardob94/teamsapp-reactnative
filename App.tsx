6b459254200095e5b60a3919be41925dec18c066
Carlos Eduardo <educonts08@gmail.com>
2024-03-09 19:27:07 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Groups } from '@screens/Groups';
+import { ThemeProvider } from 'styled-components'
+import theme from '@theme/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ThemeProvider theme={theme}>
+        <StatusBar style="light" />
+        <Groups />
+      </ThemeProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
